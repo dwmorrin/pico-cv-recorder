55f6a5b8b9c5847d375ed1a2f2ef5ef0cee72afe
David Morrin <dwmorrin@gmail.com>
2021-02-07 14:54:56 -0500

    fix: cleans up const definitions

--- dac.c
+++ dac.c
@@ -4,6 +4,8 @@
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 
+#define I2C_PORT i2c0
+
 /*
   Connections from Raspberry Pi Pico to Adafruit MCP4725 DAC breakout:
     GPIO 4 (pin 6) -> SDA (pin 4)
@@ -15,18 +17,12 @@
 // default address of MCP4725 DAC
 static int addr = 0x62;
 const uint LED_PIN = 25;
-
-#define I2C_PORT i2c0
-#define FIRST_BYTE(n) (n / 16)
-#define SECOND_BYTE(n) ((n % 16) << 4)
-#define WRITE_BYTE 0x40
-
-#define DELAY_MS 1000
-#define STEP 104
-#define DAC_MAX 4096
+const uint DELAY_MS = 1000;
+const uint STEP = 104;
+const uint DAC_MAX = 4096;
 
 static void mcp4725_write(uint value) {
-  uint8_t data[] = {WRITE_BYTE, FIRST_BYTE(value), SECOND_BYTE(value)};
+  uint8_t data[] = {0x40, value / 16, (value % 16) << 4};
   i2c_write_blocking(I2C_PORT, addr, data, 3, false);
 }
 
@@ -56,13 +52,15 @@ int main() {
   adc_gpio_init(26); // 26, 27, 28, or 29
   // select ADC input (matching what was init'd for GPIO)
   adc_select_input(0);
+  // 12-bit conversion, calibrate for V_max
+  const float conversion_factor = 3.3f / (1 << 12);
 
   uint i = 0;
   while (1) {
     printf("writing to DAC: %d\n", i);
     mcp4725_write(i);
     uint16_t reading = adc_read();
-    printf("reading from ADC: 0x%03x\n", reading);
+    printf("reading from ADC: 0x%03x, voltage: %f V\n", reading, reading * conversion_factor);
     sleep_ms(DELAY_MS);
     i = (i + STEP) % DAC_MAX;
   }
