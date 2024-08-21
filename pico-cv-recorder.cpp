bbccdf38c08bdea8d37fddd70a59dbb906d50470
David Morrin <dwmorrin@gmail.com>
2024-08-20 23:12:43 -0400

    formatter change 2 to 4 spaces
    VS Code C++ extension has decided it must be 4 spaces now.
    
    Could not find an easy way to fix it without disabling formatting.

--- pico-cv-recorder.cpp
+++ pico-cv-recorder.cpp
@@ -16,8 +16,8 @@
 
 uint semitone_quantize(uint adc_value)
 {
-  // parentheses matter: we want integer division before multiplication
-  return MAGIC_NUMBER_SEMITONE * (adc_value / MAGIC_NUMBER_SEMITONE);
+    // parentheses matter: we want integer division before multiplication
+    return MAGIC_NUMBER_SEMITONE * (adc_value / MAGIC_NUMBER_SEMITONE);
 }
 
 #define ADC_MAX_VALUE 4095
@@ -26,14 +26,14 @@ uint semitone_quantize(uint adc_value)
 // divide up the range of the ADC into NUMBER_OF_BUCKETS buckets
 int adc_to_bucket(int adc_value)
 {
-  return adc_value / (ADC_MAX_VALUE / NUMBER_OF_BUCKETS);
+    return adc_value / (ADC_MAX_VALUE / NUMBER_OF_BUCKETS);
 }
 
 int quantize_to_index_of_scale(int index, const int scale[], const int scale_size)
 {
-  int octave = index / scale_size;
-  int i = index % scale_size;
-  return octave * MAGIC_NUMBER_SEMITONE * 12 + scale[i] * MAGIC_NUMBER_SEMITONE;
+    int octave = index / scale_size;
+    int i = index % scale_size;
+    return octave * MAGIC_NUMBER_SEMITONE * 12 + scale[i] * MAGIC_NUMBER_SEMITONE;
 }
 
 #define quantize_scale(index, scale) quantize_to_index_of_scale(index, scale, SIZE_OF_ARRAY(scale))
@@ -67,10 +67,10 @@ const int scale_pentatonic[] = {0, 2, 4, 7, 9};
       CCW   -> 3.3 V (pin 36) (slowest tempo)
       WIPER -> GPIO 27 (pin 32) (ADC)
       CW    -> GND (pin 2) (fastest tempo)
-  
+
   Voltage-to-be-sampled input:
     GPIO 26 (pin 31)
-  
+
   Trigger output: (pulse high on playback)
     GPIO 15 (pin 20)
 
@@ -78,13 +78,13 @@ const int scale_pentatonic[] = {0, 2, 4, 7, 9};
     For 8 pot boards, 3 bits for pot address, plus a bit per board
     (no extra bit if just one board)
     GPIO 6, 7, 8 (pins 9, 10, 11) -> A0, A1, A2
-  
+
   Quantize mode:
     Floors ADC value to 1V/Octave semitones (1/12 V ~= 0.08333)
     Quantize function depends on DAC power supply
     High = quantize.
     GPIO 10 (pin 14)
-  
+
   Quantize range & scale:
     Range: full range or bucketed
       High = full range
@@ -95,23 +95,23 @@ const int scale_pentatonic[] = {0, 2, 4, 7, 9};
 */
 enum GPIO_PINS // hardware pin #
 {
-  QUANTIZE_RANGE_PIN = 0, // 1
-  QUANTIZE_SCALE_PIN = 1, // 2
-  DAC_SDA_PIN = 4,        // 6
-  DAC_SCL_PIN = 5,        // 7
-  POT_ADDR_0_PIN = 6,     // 9
-  POT_ADDR_1_PIN = 7,     // 10
-  POT_ADDR_2_PIN = 8,     // 11
-  QUANTIZE_PIN = 10,      // 14
-  EXT_TRIG_EN_PIN = 11,   // 15
-  TRIG_OUT_PIN = 15,      // 20
-  TRIG_BUTTON_PIN = 16,   // 21
-  MODE_BUTTON_PIN = 17,   // 22
-  TRIG_PULSE_PIN = 18,    // 24
-  MODE_PULSE_PIN = 19,    // 25
-  LED_PIN = 25,           // built-in LED
-  CV_IN_PIN = 26,         // 31
-  TEMPO_IN_PIN = 27,      // 32
+    QUANTIZE_RANGE_PIN = 0, // 1
+    QUANTIZE_SCALE_PIN = 1, // 2
+    DAC_SDA_PIN = 4,        // 6
+    DAC_SCL_PIN = 5,        // 7
+    POT_ADDR_0_PIN = 6,     // 9
+    POT_ADDR_1_PIN = 7,     // 10
+    POT_ADDR_2_PIN = 8,     // 11
+    QUANTIZE_PIN = 10,      // 14
+    EXT_TRIG_EN_PIN = 11,   // 15
+    TRIG_OUT_PIN = 15,      // 20
+    TRIG_BUTTON_PIN = 16,   // 21
+    MODE_BUTTON_PIN = 17,   // 22
+    TRIG_PULSE_PIN = 18,    // 24
+    MODE_PULSE_PIN = 19,    // 25
+    LED_PIN = 25,           // built-in LED
+    CV_IN_PIN = 26,         // 31
+    TEMPO_IN_PIN = 27,      // 32
 };
 
 // tempo constants
@@ -122,8 +122,8 @@ static struct repeating_timer tempoTimer;
 
 enum ADC_INPUTS
 {
-  ADC_IN_CV = 0,
-  ADC_IN_TEMPO = 1,
+    ADC_IN_CV = 0,
+    ADC_IN_TEMPO = 1,
 };
 
 // memory
@@ -148,8 +148,8 @@ volatile uint8_t pot_addr = 0;
 
 static void dac_write(uint value)
 {
-  uint8_t data[] = {0x40, value / 16, (value % 16) << 4};
-  i2c_write_blocking(I2C_PORT, DAC_ADDR, data, 3, false);
+    uint8_t data[] = {0x40, value / 16, (value % 16) << 4};
+    i2c_write_blocking(I2C_PORT, DAC_ADDR, data, 3, false);
 }
 
 void onTrigger();
@@ -157,48 +157,48 @@ int64_t beatTrigger(alarm_id_t, void *);
 
 void pot_address_inc()
 {
-  pot_addr = (pot_addr + 1) % 8;
-  // if multiple boards, add that logic here
+    pot_addr = (pot_addr + 1) % 8;
+    // if multiple boards, add that logic here
 }
 
 void pot_address_setup()
 {
-  gpio_init(POT_ADDR_0_PIN);
-  gpio_set_dir(POT_ADDR_0_PIN, GPIO_OUT);
-  gpio_init(POT_ADDR_1_PIN);
-  gpio_set_dir(POT_ADDR_1_PIN, GPIO_OUT);
-  gpio_init(POT_ADDR_2_PIN);
-  gpio_set_dir(POT_ADDR_2_PIN, GPIO_OUT);
+    gpio_init(POT_ADDR_0_PIN);
+    gpio_set_dir(POT_ADDR_0_PIN, GPIO_OUT);
+    gpio_init(POT_ADDR_1_PIN);
+    gpio_set_dir(POT_ADDR_1_PIN, GPIO_OUT);
+    gpio_init(POT_ADDR_2_PIN);
+    gpio_set_dir(POT_ADDR_2_PIN, GPIO_OUT);
 }
 
 void set_pot_address()
 {
-  gpio_put(POT_ADDR_0_PIN, pot_addr & 0x01);
-  gpio_put(POT_ADDR_1_PIN, pot_addr & 0x02);
-  gpio_put(POT_ADDR_2_PIN, pot_addr & 0x04);
+    gpio_put(POT_ADDR_0_PIN, pot_addr & 0x01);
+    gpio_put(POT_ADDR_1_PIN, pot_addr & 0x02);
+    gpio_put(POT_ADDR_2_PIN, pot_addr & 0x04);
 }
 
 int64_t beatAnticipate(alarm_id_t id, void *user_data)
 {
-  gpio_put(LED_PIN, false);
-  internalClockAlarmId = add_alarm_in_ms(tempoDelayMs, &beatTrigger, 0, true);
-  return 0;
+    gpio_put(LED_PIN, false);
+    internalClockAlarmId = add_alarm_in_ms(tempoDelayMs, &beatTrigger, 0, true);
+    return 0;
 }
 
 int64_t beatTrigger(alarm_id_t id, void *user_data)
 {
-  onTrigger();
-  gpio_put(LED_PIN, true);
-  if (!externalTrigger)
-    internalClockAlarmId = add_alarm_in_ms(tempoDelayMs, &beatAnticipate, 0, true);
-  return 0;
+    onTrigger();
+    gpio_put(LED_PIN, true);
+    if (!externalTrigger)
+        internalClockAlarmId = add_alarm_in_ms(tempoDelayMs, &beatAnticipate, 0, true);
+    return 0;
 }
 
 int64_t pinOff(alarm_id_t id, void *user_data)
 {
-  uint pin = (uint)user_data;
-  gpio_put(pin, false);
-  return 0;
+    uint pin = (uint)user_data;
+    gpio_put(pin, false);
+    return 0;
 }
 
 void onPulse(uint);
@@ -206,193 +206,195 @@ void onEdge(uint, uint32_t);
 
 void enableInput(uint pin)
 {
-  gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE, true, &onEdge);
+    gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE, true, &onEdge);
 }
 
 void disableInput(uint pin)
 {
-  gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, false);
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, false);
 }
 
 // debouncing: edge calls this, checks if button held high, runs trig or resets interrupt
 int64_t checkTrigger(alarm_id_t id, void *user_data)
 {
-  uint gpio = (uint)user_data;
-  if (gpio_get(gpio))
-    onPulse(gpio);
-  else
-    enableInput(gpio);
-  return 0;
+    uint gpio = (uint)user_data;
+    if (gpio_get(gpio))
+        onPulse(gpio);
+    else
+        enableInput(gpio);
+    return 0;
 }
 
 // debouncing: immediately react for pulse inputs, debounce manually pressed buttons
 void onEdge(uint gpio, uint32_t events)
 {
-  switch (gpio)
-  {
-  case TRIG_PULSE_PIN:
-  case MODE_PULSE_PIN:
-    // no debounce
-    onPulse(gpio);
-    break;
-  case TRIG_BUTTON_PIN:
-  case MODE_BUTTON_PIN:
-    // software debounce
-    disableInput(gpio);
-    add_alarm_in_ms(20, &checkTrigger, (void *)gpio, true);
-    break;
-  }
+    switch (gpio)
+    {
+    case TRIG_PULSE_PIN:
+    case MODE_PULSE_PIN:
+        // no debounce
+        onPulse(gpio);
+        break;
+    case TRIG_BUTTON_PIN:
+    case MODE_BUTTON_PIN:
+        // software debounce
+        disableInput(gpio);
+        add_alarm_in_ms(20, &checkTrigger, (void *)gpio, true);
+        break;
+    }
 }
 
 // debouncing: this runs post-debounce check
 void onPulse(uint gpio)
 {
-  switch (gpio)
-  {
-  case TRIG_BUTTON_PIN:
-  case TRIG_PULSE_PIN:
-    triggered = true;
-    if (gpio == TRIG_BUTTON_PIN)
-      enableInput(gpio);
-    break;
-  case MODE_BUTTON_PIN:
-  case MODE_PULSE_PIN:
-    modeToggled = true;
-    if (gpio == MODE_BUTTON_PIN)
-      enableInput(gpio);
-    break;
-  }
+    switch (gpio)
+    {
+    case TRIG_BUTTON_PIN:
+    case TRIG_PULSE_PIN:
+        triggered = true;
+        if (gpio == TRIG_BUTTON_PIN)
+            enableInput(gpio);
+        break;
+    case MODE_BUTTON_PIN:
+    case MODE_PULSE_PIN:
+        modeToggled = true;
+        if (gpio == MODE_BUTTON_PIN)
+            enableInput(gpio);
+        break;
+    }
 }
 
 void resetInternalClock()
 {
-  if (internalClockAlarmId)
-    cancel_alarm(internalClockAlarmId);
-  gpio_put(LED_PIN, 0);
-  if (!externalTrigger)
-    internalClockAlarmId = add_alarm_in_ms(0, &beatTrigger, 0, true);
+    if (internalClockAlarmId)
+        cancel_alarm(internalClockAlarmId);
+    gpio_put(LED_PIN, 0);
+    if (!externalTrigger)
+        internalClockAlarmId = add_alarm_in_ms(0, &beatTrigger, 0, true);
 }
 
 // update tempo potentiometer reading; probably not quite right yet with the reset
 bool updateTempoDelay(repeating_timer_t *rt)
 {
-  adc_select_input(ADC_IN_TEMPO);
-  uint16_t tempoRaw = adc_read(); // 0 to 4096
-  // divide desired tempo by two; two delays per beat
-  uint newTempoDelayMs = ((SLOW_MS - FAST_MS) * tempoRaw / 4096 + FAST_MS) / 2;
-  if (newTempoDelayMs > tempoDelayMs + 20 || newTempoDelayMs < tempoDelayMs - 20)
-  {
-    tempoDelayMs = newTempoDelayMs;
-  }
-  return true;
+    adc_select_input(ADC_IN_TEMPO);
+    uint16_t tempoRaw = adc_read(); // 0 to 4096
+    // divide desired tempo by two; two delays per beat
+    uint newTempoDelayMs = ((SLOW_MS - FAST_MS) * tempoRaw / 4096 + FAST_MS) / 2;
+    if (newTempoDelayMs > tempoDelayMs + 20 || newTempoDelayMs < tempoDelayMs - 20)
+    {
+        tempoDelayMs = newTempoDelayMs;
+    }
+    return true;
 }
 
 void onTrigger()
 {
-  // write to memory if recording
-  if (recording)
-  {
-    gpio_put(LED_PIN, true);
-    if (externalTrigger)
-      add_alarm_in_ms(20, &pinOff, (void *)LED_PIN, true);
-    adc_select_input(ADC_IN_CV);
-    memory[memoryIndex] = adc_read();
-  }
-  // write to DAC
-  if (quantize)
-  {
-    if (quantize_full_range)
-      dac_write(semitone_quantize(memory[memoryIndex]));
-    else
+    // write to memory if recording
+    if (recording)
     {
-      int bucket = adc_to_bucket(memory[memoryIndex]);
-      int quantized_value;
-      if (quantize_pentatonic) quantized_value = quantize_scale(bucket, scale_pentatonic);
-      else quantized_value = quantize_scale(bucket, scale_major);
-      dac_write(quantized_value);
+        gpio_put(LED_PIN, true);
+        if (externalTrigger)
+            add_alarm_in_ms(20, &pinOff, (void *)LED_PIN, true);
+        adc_select_input(ADC_IN_CV);
+        memory[memoryIndex] = adc_read();
     }
-  }
-  else
-    dac_write(memory[memoryIndex]);
-  // trigger output
-  gpio_put(TRIG_OUT_PIN, true);
-  add_alarm_in_ms(10, &pinOff, (void *)TRIG_OUT_PIN, true);
-  // advance memory
-  memoryIndex = (memoryIndex + 1) % MEMORY_LENGTH;
-  // advance pot address
-  pot_address_inc();
-  set_pot_address();
-  // update state
-  triggered = false;
+    // write to DAC
+    if (quantize)
+    {
+        if (quantize_full_range)
+            dac_write(semitone_quantize(memory[memoryIndex]));
+        else
+        {
+            int bucket = adc_to_bucket(memory[memoryIndex]);
+            int quantized_value;
+            if (quantize_pentatonic)
+                quantized_value = quantize_scale(bucket, scale_pentatonic);
+            else
+                quantized_value = quantize_scale(bucket, scale_major);
+            dac_write(quantized_value);
+        }
+    }
+    else
+        dac_write(memory[memoryIndex]);
+    // trigger output
+    gpio_put(TRIG_OUT_PIN, true);
+    add_alarm_in_ms(10, &pinOff, (void *)TRIG_OUT_PIN, true);
+    // advance memory
+    memoryIndex = (memoryIndex + 1) % MEMORY_LENGTH;
+    // advance pot address
+    pot_address_inc();
+    set_pot_address();
+    // update state
+    triggered = false;
 }
 
 void onRecPlayToggle()
 {
-  recording = !recording;
-  resetInternalClock();
-  modeToggled = false;
+    recording = !recording;
+    resetInternalClock();
+    modeToggled = false;
 }
 
 int main()
 {
-  // register front panel controls with callbacks
-  enableInput(TRIG_BUTTON_PIN);
-  enableInput(MODE_BUTTON_PIN);
-  enableInput(TRIG_PULSE_PIN);
-  enableInput(MODE_PULSE_PIN);
-  gpio_init(QUANTIZE_PIN);
-  gpio_set_dir(QUANTIZE_PIN, GPIO_IN);
-  gpio_init(QUANTIZE_RANGE_PIN);
-  gpio_set_dir(QUANTIZE_RANGE_PIN, GPIO_IN);
-  gpio_init(QUANTIZE_SCALE_PIN);
-  gpio_set_dir(QUANTIZE_SCALE_PIN, GPIO_IN);
-  gpio_init(EXT_TRIG_EN_PIN);
-  gpio_set_dir(EXT_TRIG_EN_PIN, GPIO_IN);
+    // register front panel controls with callbacks
+    enableInput(TRIG_BUTTON_PIN);
+    enableInput(MODE_BUTTON_PIN);
+    enableInput(TRIG_PULSE_PIN);
+    enableInput(MODE_PULSE_PIN);
+    gpio_init(QUANTIZE_PIN);
+    gpio_set_dir(QUANTIZE_PIN, GPIO_IN);
+    gpio_init(QUANTIZE_RANGE_PIN);
+    gpio_set_dir(QUANTIZE_RANGE_PIN, GPIO_IN);
+    gpio_init(QUANTIZE_SCALE_PIN);
+    gpio_set_dir(QUANTIZE_SCALE_PIN, GPIO_IN);
+    gpio_init(EXT_TRIG_EN_PIN);
+    gpio_set_dir(EXT_TRIG_EN_PIN, GPIO_IN);
 
-  // pot board addressing
-  pot_address_setup();
+    // pot board addressing
+    pot_address_setup();
 
-  // i2c setup for DAC
-  i2c_init(I2C_PORT, 400e3);
-  gpio_set_function(DAC_SDA_PIN, GPIO_FUNC_I2C);
-  gpio_set_function(DAC_SCL_PIN, GPIO_FUNC_I2C);
-  gpio_pull_up(DAC_SDA_PIN);
-  gpio_pull_up(DAC_SCL_PIN);
+    // i2c setup for DAC
+    i2c_init(I2C_PORT, 400e3);
+    gpio_set_function(DAC_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(DAC_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(DAC_SDA_PIN);
+    gpio_pull_up(DAC_SCL_PIN);
 
-  // ADC setup
-  adc_init();
-  adc_gpio_init(CV_IN_PIN);
-  adc_gpio_init(TEMPO_IN_PIN);
+    // ADC setup
+    adc_init();
+    adc_gpio_init(CV_IN_PIN);
+    adc_gpio_init(TEMPO_IN_PIN);
 
-  // trigger output
-  gpio_init(TRIG_OUT_PIN);
-  gpio_set_dir(TRIG_OUT_PIN, GPIO_OUT);
-  gpio_put(TRIG_OUT_PIN, false);
+    // trigger output
+    gpio_init(TRIG_OUT_PIN);
+    gpio_set_dir(TRIG_OUT_PIN, GPIO_OUT);
+    gpio_put(TRIG_OUT_PIN, false);
 
-  // tempo indicator LED
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
-  resetInternalClock();
+    // tempo indicator LED
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    resetInternalClock();
 
-  add_repeating_timer_ms(TEMPO_READ_DELAY, &updateTempoDelay, 0, &tempoTimer);
+    add_repeating_timer_ms(TEMPO_READ_DELAY, &updateTempoDelay, 0, &tempoTimer);
 
-  while (true)
-  {
-    // check for external trigger event
-    if (triggered)
-      onTrigger();
-    // check for rec/play event
-    if (modeToggled)
-      onRecPlayToggle();
-    // check state of hardware latched switches
-    quantize = gpio_get(QUANTIZE_PIN);
-    quantize_full_range = gpio_get(QUANTIZE_RANGE_PIN);
-    quantize_pentatonic = gpio_get(QUANTIZE_SCALE_PIN);
-    bool extTrigEn = gpio_get(EXT_TRIG_EN_PIN);
-    if (extTrigEn != externalTrigger)
+    while (true)
     {
-      externalTrigger = extTrigEn;
-      resetInternalClock();
+        // check for external trigger event
+        if (triggered)
+            onTrigger();
+        // check for rec/play event
+        if (modeToggled)
+            onRecPlayToggle();
+        // check state of hardware latched switches
+        quantize = gpio_get(QUANTIZE_PIN);
+        quantize_full_range = gpio_get(QUANTIZE_RANGE_PIN);
+        quantize_pentatonic = gpio_get(QUANTIZE_SCALE_PIN);
+        bool extTrigEn = gpio_get(EXT_TRIG_EN_PIN);
+        if (extTrigEn != externalTrigger)
+        {
+            externalTrigger = extTrigEn;
+            resetInternalClock();
+        }
     }
-  }
 }
